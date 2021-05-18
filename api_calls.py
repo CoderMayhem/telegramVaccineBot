2fabaf645b423125320d25b44581d9a625bc8f2d
CoderMayhem <namanmishra1900@gmail.com>
2021-05-18 21:32:12 +0530

    bfix

--- api_calls.py
+++ api_calls.py
@@ -8,8 +8,8 @@ def getSessionsByPin(pincode):
     url = const.serverUrl + '/v2/appointment/sessions/public/findByPin?pincode=' + str(pincode) + '&date=' + str(date)
     headers_dict = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
     response = requests.get(url, headers= headers_dict)
-    print(response.status_code)
-    sessions = response.json().type('utf-8')
+    print('here?' + str(response.status_code))
+    sessions = response.json()
     return sessions
 
 def getCalendarByPin(pincode):
@@ -26,3 +26,7 @@ def getDate():
     date_time = now.strftime("%d-%m-20%y")
     return str(date_time)
 
+def main():
+    getSessionsByPin(110001)
+
+main()
