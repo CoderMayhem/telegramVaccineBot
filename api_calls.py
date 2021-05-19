d7ca1affb32f93cc1eb6ae6fbd6f5a1ce074ed02
CoderMayhem <namanmishra1900@gmail.com>
2021-05-20 02:32:56 +0530

    new

--- api_calls.py
+++ api_calls.py
@@ -8,9 +8,9 @@ def getSessionsByPin(pincode):
     date = getDate()
     print(date)
     url = const.serverUrl + '/v2/appointment/sessions/public/findByPin?pincode=' + str(pincode) + '&date=' + str(date)
-    headers_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+    #headers_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
     print(url)
-    response = requests.get(url, headers= headers_dict)
+    response = requests.get(url)#, headers= headers_dict)
     print('this is response : ' + str(response))
     print('here?' + str(response.status_code))
     sessions = json.loads(response.text)
