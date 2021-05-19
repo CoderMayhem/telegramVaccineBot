8da50b82678dacc19836a640e53d475e38f74cd4
CoderMayhem <namanmishra1900@gmail.com>
2021-05-20 01:16:09 +0530

    bfix

--- api_calls.py
+++ api_calls.py
@@ -2,6 +2,7 @@ import constants as const
 from datetime import datetime
 import re
 import requests
+import json
 
 def getSessionsByPin(pincode):
     date = getDate()
@@ -9,7 +10,7 @@ def getSessionsByPin(pincode):
     headers_dict = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
     response = requests.get(url, headers= headers_dict)
     print('here?' + str(response.status_code))
-    sessions = response.json()
+    sessions = json.loads(response.text)
     return sessions
 
 def getCalendarByPin(pincode):
